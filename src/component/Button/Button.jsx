dad91d675de16bacf309c281a86b4b4887b43f50
Salah Al-Dhaferi <binaryminded@gmail.com>
2023-02-23 20:48:11 +0300

    init commit

---
+++ src/component/Button/Button.jsx
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const Button = styled.button`
+background: #112f64;
+color: #fff;
+padding: 10px 15px;
+border-radius: 10px;
+font-size: 20px;
+display: inline;
+`
